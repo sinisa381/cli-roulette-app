378577322188cc08f0a1309c39736404a23d81dc
sinisa <sinisa820@gmail.com>
2021-01-23 18:29:24 +0100

    add roulette

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, useEffect, useRef, useState } from 'react';
+import * as THREE from 'three';
+import { AnimationClip, AnimationMixer } from 'three';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { useGLTF, OrbitControls } from 'drei';
+import { useAnimations } from '@react-three/drei/useAnimations';
+import Anim from './Anim';
+
+const namebre = 'Dummy002.quaternion';
+const possibleAnimations = [
+  '4',
+  'random1',
+  'dontmove1',
+  'ruletrotate',
+  'dontmove2',
+  'random2',
+  'dontmove3',
+  'dontmove4',
+  'random3',
+];
+
+function Model() {
+  const group = useRef();
+  const { nodes, materials, scene, animations } = useGLTF('/anim.gltf');
+  const initTracks = animations[0].tracks;
+  const clip = animations[0];
+  clip.tracks = [initTracks[8], initTracks[3]];
+
+  console.log(clip);
+  const [mixer] = useState(() => new THREE.AnimationMixer());
+  useEffect(() => void mixer.clipAction(clip, group.current).play(), []);
+  useFrame((state, delta) => {
+    mixer.update(delta);
+  });
+  return (
+    <primitive
+      ref={group}
+      scale={[7, 7, 7]}
+      position={[-3, 9, 0]}
+      rotation={[Math.PI / 2, 0, 0]}
+      object={scene}
+      dispose={null}
+    />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      camera={{
+        position: [0, 20, 80],
+        fov: 20,
+        focus: 10,
+        far: 1000,
+        filmGauge: 35,
+        filmOffset: 0,
+      }}
+      colorManagement
+    >
+      <ambientLight intensity={0.5} />
+      <OrbitControls />
+      <Suspense fallback={null}>
+        <Model />
+        {/* <Anim /> */}
+      </Suspense>
+      {/* <mesh>
+        <boxBufferGeometry attach="geometry" />
+        <meshBasicMaterial attach="material" />
+      </mesh> */}
+    </Canvas>
   );
 }
 
